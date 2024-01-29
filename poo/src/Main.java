18126094af7e10b2b88ef2bbfe4b144f73d850dc
Lab A2 <jdencalahuaya@gmail.com>
2024-01-29 11:31:59 -0500

    Add(II): Add PrintSquare Function

--- poo/src/Main.java
+++ poo/src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        printSquare(12);
+        System.out.println("-----------------");
+        printSquare(8);
+    }
+
+    public static void printSquare(int length){
+        //for that prints width
+        for(int i = 1; i <= length; i++) {
+            //for that prints height
+            for(int j = 1; j <= length; j++){
+                System.out.print(" * ");
+            }
+            System.out.print("\n");
+        }
     }
 }
